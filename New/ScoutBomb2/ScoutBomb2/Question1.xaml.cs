b2c91afd2a92c125766693d922db0461b901f153
Anders Poulsen <and.poulsen@gmail.com>
2016-10-24 23:33:03 +0200

    New new bomb

---
+++ New/ScoutBomb2/ScoutBomb2/Question1.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ScoutBomb2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Question1 : Page
+    {
+        public Question1()
+        {
+            InitializeComponent();
+            var timeLeft = ((App)Application.Current).TimeLeft = new TimeSpan(0, 25, 0);
+            txbTimeLeft.Text = timeLeft.ToString();
+        }
+    }
+}
